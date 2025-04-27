6b3e3bf51795724119edcbde030f31b89b213d48
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-04-27 20:32:19 +0300

    adding redux store and provide

---
+++ src/store/fetchPosts.ts
@@ -0,0 +1,19 @@
+import { AppDispatch } from "./store";
+import { fetchPosts as fetchPostsApi } from "../api/jsonplaceholder/index";
+import {
+  setPostsLoading,
+  setPostsSuccess,
+  setPostsFailure,
+} from "./slices/postsSlice";
+
+export const fetchPosts = () => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(setPostsLoading());
+    const posts = await fetchPostsApi();
+    dispatch(setPostsSuccess(posts));
+  } catch (error) {
+    dispatch(
+      setPostsFailure(error instanceof Error ? error.message : "Unknown error")
+    );
+  }
+};
---
+++ src/store/slices/postsSlice.ts
@@ -0,0 +1,90 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Post } from "../../api/jsonplaceholder/posts";
+
+type ReactionType = "like" | "dislike";
+type UserReaction = ReactionType | undefined;
+
+interface Reactions {
+  likes: number;
+  dislikes: number;
+  userReaction: UserReaction;
+}
+
+interface PostsState {
+  posts: Post[];
+  loading: boolean;
+  error: string | null;
+  reactions: Record<number, Reactions>;
+}
+
+const initialState: PostsState = {
+  posts: [],
+  loading: false,
+  error: null,
+  reactions: {},
+};
+
+const postsSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    setPostsLoading(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    setPostsSuccess(state, action: PayloadAction<Post[]>) {
+      state.loading = false;
+      state.posts = action.payload;
+
+      action.payload.forEach((post) => {
+        if (state.reactions[post.id]) {
+          state.reactions[post.id] = {
+            likes: Math.floor(Math.random() * 50),
+            dislikes: Math.floor(Math.random() * 50),
+            userReaction: undefined,
+          };
+        }
+      });
+    },
+    setPostsFailure(state, acion: PayloadAction<string>) {
+      state.loading = false;
+      state.error = acion.payload;
+    },
+    setReaction(
+      state,
+      action: PayloadAction<{ postId: number; reaction: ReactionType }>
+    ) {
+      const { postId, reaction } = action.payload;
+      if (!state.reactions[postId]) {
+        state.reactions[postId] = {
+          likes: reaction === "like" ? 1 : 0,
+          dislikes: reaction === "dislike" ? 1 : 0,
+          userReaction: reaction,
+        };
+        return;
+      }
+
+      const current = state.reactions[postId];
+
+      if (current.userReaction === reaction) {
+        current[reaction === "like" ? "likes" : "dislikes"] -= 1;
+        current.userReaction = undefined;
+      } else if (current.userReaction) {
+        current[current.userReaction === "like" ? "likes" : "dislikes"] -= 1;
+        current[reaction === "like" ? "likes" : "dislikes"] += 1;
+        current.userReaction = reaction;
+      } else {
+        current[reaction === "like" ? "likes" : "dislikes"] += 1;
+        current.userReaction = reaction;
+      }
+    },
+  },
+});
+
+export const {
+  setPostsLoading,
+  setPostsSuccess,
+  setPostsFailure,
+  setReaction,
+} = postsSlice.actions;
+export default postsSlice.reducer;
